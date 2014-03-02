90811b9147867efee201c4d68950e74a7908cf87
amit-git <webprogrammer.amit@gmail.com>
2014-03-02 09:42:26 -0800

    removing bad chars in js file
    Not sure where did they come from.

--- pytheas-helloworld/src/main/resources/helloworld/index.js
+++ pytheas-helloworld/src/main/resources/helloworld/index.js
@@ -10,8 +10,8 @@ $(document).ready(function() {
 
     var oTable = $('#country-table').dataTable( {
         "aoColumns": [
-            { "sTitle": "Code",  "mDataProp" : "code", "sWidth" : "10%", "sDefaultContent": "-" },
-            { "sTitle": "Country",     "mDataProp" : "name", "sWidth" : "90%", "sDefaultContent": "-" }
+            { "sTitle": "Code", "mDataProp" : "code", "sWidth" : "10%", "sDefaultContent": "-" },
+            { "sTitle": "Country", "mDataProp" : "name", "sWidth" : "90%", "sDefaultContent": "-" }
         ],
         "sAjaxDataProp": "countries",
         "sAjaxSource": "list",
